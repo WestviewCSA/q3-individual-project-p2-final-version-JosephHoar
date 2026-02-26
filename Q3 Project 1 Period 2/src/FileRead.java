c99b013cdd6459134e44469adc95c71c5d71c35a
1929171 <1929171@01-18133.pusd.dom>
2026-02-26 11:53:29 -0800

    started queue error with queue being empty

--- Q3 Project 1 Period 2/src/FileRead.java
+++ Q3 Project 1 Period 2/src/FileRead.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class FileRead {
@@ -10,8 +12,8 @@ public class FileRead {
 		if(args.length > 0) {
 			File mapa = new File(args[0]);
 			String[][][] coorda = file(mapa);
-			co(mapa);
 			if(coorda != null) {
+				queueRoute(co(mapa));
 				cords(coorda);
 			}
 		}else {
@@ -59,22 +61,24 @@ public class FileRead {
 		return b;
 		
 	}
-	public static ArrayList<String> co(File map){
+	public static String[][][] co(File map){
 		try {
 			Scanner scan = new Scanner(map);
 			int h = scan.nextInt();
 			int w = scan.nextInt();
 			int l = scan.nextInt();
-			ArrayList<String> res = new ArrayList<String>();
+			String[][][] res = new String[h][w][l];
 			for(int i = 0; i < h*l + l; i++) {
 				scan.nextLine();
 			}
-			while(scan.hasNext()) {
-				String c = scan.next() + " " + scan.nextInt() + " " + scan.nextInt() + " " + scan.nextInt();
-				res.add(c);
-				System.out.println(c);
+			for(int i = 0; i < l; i++) {
+				for(int j = 0; j < h; j++) {
+					for(int k = 0; k < w; k++) {
+						String s = scan.next() + " " + j + " " + k + " " + i;
+						res[j][k][i] = s;
+					}
+				}
 			}
-			System.out.println();
 			return res;
 			
 		} catch (FileNotFoundException e) {
@@ -83,6 +87,96 @@ public class FileRead {
 			return null;
 		}
 	
+	}
+	public static ArrayList<String> queueRoute(String[][][] map){
+		ArrayList<String> visited = new ArrayList<String>();
+		Queue<String> queue = new LinkedList<>();
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int d = 0;
+		boolean start = false;
+		boolean found = false;
+		while(!found) {
+			while(!start) {
+				for(int i = 0; i < map[0][0].length; i++) {
+					for(int j = 0; j < map.length; j++) {
+						for(int k = 0; k < map[0].length; k++) {
+							if(map[j][k][i].startsWith("W") ) {
+								queue.add(map[j][k][i]);
+								a = j;
+								b = k;
+								c = i;
+								break;
+							}
+						}
+					}
+				}
+				start = true;
+			}
+			visited.add(queue.remove());
+			System.out.println(visited.get(d));
+			String[] e = visited.get(d).split(" ");
+			a = Integer.parseInt(e[1]);
+			b = Integer.parseInt(e[2]);
+			c = Integer.parseInt(e[3]);
+			d++;
+			if(a-1>= 0) {
+				if(!map[a-1][b][c].startsWith("@") && a-1>= 0 && !visited.contains(map[a-1][b][c])) {
+					if(map[a-1][b][c].startsWith("$")) {
+						visited.add(map[a-1][b][c]);
+						found = true;
+					}else if(map[a-1][b][c].startsWith("|")) {
+						visited.add(map[a-1][b][c]);
+						start = false;
+					}else{
+						queue.add(map[a-1][b][c]);
+					}
+				}
+			}
+			if(a + 1 < map.length) {
+				if(!map[a+1][b][c].startsWith("@") && a + 1 < map.length && !visited.contains(map[a+1][b][c])) {
+					if(map[a+1][b][c].startsWith("$")) {
+						visited.add(map[a+1][b][c]);
+						found = true;
+					}else if(map[a+1][b][c].startsWith("|")) {
+						queue.add(map[a+1][b][c]);
+						start = false;
+					}else {
+						queue.add(map[a+1][b][c]);
+					}
+				}
+			}
+			if(b + 1 <map[0].length) {
+				if(!map[a][b+1][c].startsWith("@") && b + 1 <map[0].length && !visited.contains(map[a][b+1][c])) {
+					if(map[a][b+1][c].startsWith("$")) {
+						visited.add(map[a][b+1][c]);
+						found = true;
+					}else if(map[a][b+1][c].startsWith("|")) {
+						queue.add(map[a][b+1][c]);
+						start = false;
+					}else {
+						queue.add(map[a][b+1][c]);
+					}
+				}
+			}
+			if(b-1 > 0) {
+				if(!map[a][b-1][c].startsWith("@") && !visited.contains(map[a][b-1][c])) {
+					if(map[a][b-1][c].startsWith("$")) {
+						visited.add(map[a][b-1][c]);
+						found = true;
+					}else if(map[a][b-1][c].startsWith("|")) {
+						queue.add(map[a][b-1][c]);
+						start = false;
+					}else {
+						queue.add(map[a][b-1][c]);
+					}
+				}
+			}
+			
+		}
+		System.out.println(visited);
+		return visited;
 	}
 
 }
